3335f05c4404006af83710e8a06df16022ae1bf1
Mo Amir <mo.amir.code@gmail.com>
2025-09-19 09:59:24 +0530

    fix(gemini): replaced google gemini sdk with google gemini api

--- src/services/gemini/index.ts
+++ src/services/gemini/index.ts
@@ -1,18 +1,62 @@
-import { ai } from "../../config/gemini.js";
+// import { ai } from "../../config/gemini.js";
+// import { CalculateScoreOfLeadsWithAIResponseType } from "../../types/controllers/controllers/leads/index.js";
+// import { CalculateLeadScoreWithAIType } from "../../types/services/gemini/index.js";
+
+// const calculateLeadScoreWithAI = async ({
+//   query,
+// }: CalculateLeadScoreWithAIType): Promise<CalculateScoreOfLeadsWithAIResponseType> => {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: query,
+//   });
+
+//   const cleaned = response.text?.replace(/```json\n?|\n```/g, "");
+
+//   // Parse into an object
+//   const output = JSON.parse(cleaned || "{}");
+
+//   return output;
+// };
+
+// export { calculateLeadScoreWithAI };
+
+import axios from "axios";
 import { CalculateScoreOfLeadsWithAIResponseType } from "../../types/controllers/controllers/leads/index.js";
 import { CalculateLeadScoreWithAIType } from "../../types/services/gemini/index.js";
+import { ENV_VARS } from "../../config/constants.js";
+
+const url =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
 
 const calculateLeadScoreWithAI = async ({
   query,
 }: CalculateLeadScoreWithAIType): Promise<CalculateScoreOfLeadsWithAIResponseType> => {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: query,
-  });
+  const response = await axios.post(
+    url,
+    {
+      contents: [
+        {
+          parts: [
+            {
+              text: query,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-goog-api-key": ENV_VARS.GEMINI_API_KEY || "",
+      },
+    }
+  );
 
-  const cleaned = response.text?.replace(/```json\n?|\n```/g, "");
+  const aiResponse = JSON.parse(JSON.stringify(response.data));
+  const result = aiResponse.candidates[0].content.parts[0].text;
 
-  // Parse into an object
+  const cleaned = result?.replace(/```json\n?|\n```/g, "");
+
   const output = JSON.parse(cleaned || "{}");
 
   return output;
